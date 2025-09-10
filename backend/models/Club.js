ef4d2e6c8f86efee486fb998b3093d9e124b7dc0
Lloyd Hangoma <hangoma30@gmail.com>
2025-09-10 14:19:48 +0200

    chore: remove legacy backend and ignore legacy folders/uploads

--- backend/models/Club.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const clubSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  logo: { type: String },
-  adminUser: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-});
-
-module.exports = mongoose.model('Club', clubSchema);
